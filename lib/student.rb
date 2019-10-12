b3966d7fb0eacb199c1e1d2a29ae4dc3a10a0f77
Andrew Williams <1williamsaj@gmail.com>
2019-10-12 15:37:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student < User # Inherits the information from the 'User' class
     @knowledge = [] # When we make a new student, that instance of a student will create an array tied to that specific student
   end
 
-  def learn(new_knowledge) # Takes in a string through the variable 'new_knowledge' and pushes it into our 'knowledge' array, then returning it.
+  def learn(new_knowledge) # Takes in a string through the variable 'new_knowledge' and pushes it into our 'knowledge' array.
     @knowledge << new_knowledge
   end
 
